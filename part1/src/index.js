88abb832663df875b11775c121b2ab0c9654e848
dredly <dredly@gmail.com>
2020-06-05 18:42:45 +0100

    Exercise 1.12

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,71 +1,37 @@
 import React, { useState} from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = ({headerText}) => (<h1>{headerText}</h1>)	
-
 const Button =({ onClick, text }) => (
 	<button onClick={onClick}>	
 		{text}
 	</button>
 )
 
-const Stat = ({ statText, statNum }) => (
-	<tr><td>{statText}</td><td>{statNum}</td></tr>
-)
+const App = ({anecdotes}) => {
+  const [selected, setSelected] = useState(0)
+    
+  return (
+	<div>
+	  {anecdotes[selected]}
+	  <div>
+		<Button onClick={() => setSelected(Math.floor(Math.random()*anecdotes.length))} text='next anecdote' />
+	  </div>
+    </div>		
+  )
+}
 
-const Statistics = ({ good, neutral, bad }) => {
-	const All = () => (good + neutral + bad)
-	const Avg = () => ((good - bad) / (good  + neutral + bad))
-	const Positive = () => 
-		((100 * (good / (good + neutral + bad))).toString() + '%')
-	
-	if (good + neutral + bad === 0) {
-		return (
-			<p>No feedback given</p>
-		)
-	}
-	
-	return (	
-		<>
-			<table>
-				<tbody>
-					<Stat statText='good' statNum={good} />
-					<Stat statText='neutral' statNum={neutral} />
-					<Stat statText='bad' statNum={bad} />
-					<Stat statText='All' statNum={All()} />
-					<Stat statText='Average' statNum={Avg()} />
-					<Stat statText='Positive' statNum={Positive()} />
-				</tbody>
-			</table>
-		</>
-	)
-}
-	
-const App = () => {
-	// Save clicks of each button to own state
-	const [good, setGood] = useState(0)
-	const [neutral, setNeutral] = useState(0)
-	const [bad, setBad] = useState(0)
-	
-	const addGood = () => setGood(good + 1)
-	const addNeutral = () => setNeutral(neutral + 1)
-	const addBad = () => setBad(bad + 1)
-	
-	return (
-		<div>
-			<Header headerText='give feedback' />
-			<Button onClick={addGood} text='good' />
-			<Button onClick={addNeutral} text='neutral' />
-			<Button onClick={addBad} text='bad' />
-			<Header headerText='statistics' />
-			<Statistics good={good} neutral={neutral} bad={bad} />
-		</div>
-	)
-}
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-	<App />,
-	document.getElementById('root')
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
 )
 
 
