9ef50f18215dfd7c4a02b0508694e308208547ad
dredly <dredly@gmail.com>
2020-06-08 17:07:35 +0100

    Exercise 2.6

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,17 +1,43 @@
-import React from 'react'
-import Note from './components/Note'
+import React, { useState } from 'react'
 
-const App = ({ notes }) => {
+const Person = ({personObj}) => (<div>{personObj.name}</div>)
+
+const App = () => {
+	const [ persons, setPersons ] = useState([
+		{ name: 'Arto Hellas' }
+	])
+	const [ newName, setNewName ] = useState('')
+	
+	const addPerson = (event) => {
+		event.preventDefault()
+		const personObject = {
+			name: newName,
+		}
+		setPersons(persons.concat(personObject))
+		setNewName('')
+	}
+	
+	const handlePerson = (event) => {
+		console.log(event.target.value)
+		setNewName(event.target.value)
+	} 
+	
 	return (
 		<div>
-			<h1>Notes</h1>
-			<ul>
-			{notes.map(note => 
-				<Note key={note.id} note={note} />
-			)}
-			</ul>
+			<h2>Phonebook</h2>
+			<form onSubmit={addPerson}>
+				<div>
+					name: <input value={newName} onChange={handlePerson}/>
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+				{persons.map(person => <Person key={person.name} personObj={person} />)}
 		</div>
 	)
+	
 }
 
 export default App
