415cf792e85623c5d7614587c606fe0148859886
dredly <dredly@gmail.com>
2020-06-08 15:09:53 +0100

    exercises 2.2 and 2.3

--- part2/src/index.js
+++ part2/src/index.js
@@ -7,9 +7,21 @@ const Part = ({ partName, exercises }) => (
 	</>
 )
 
-const Content = ( {parts} ) => (
+const Total = ({parts}) => {
+	console.log({parts})
+	const numExs = parts.map(part => part.exercises)
+	const total = numExs.reduce((a, b) => a + b, 0)
+	return (
+		<>
+			<strong>total of {total} exercises</strong>
+		</>
+	)
+}
+
+const Content = ({parts}) => (
 	<>
 		{parts.map(part => <Part key={part.id} partName={part.name} exercises = {part.exercises} />)}
+		<Total parts={parts} />
 	</>
 )
 
