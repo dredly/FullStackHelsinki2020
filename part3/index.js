285962684747f5b209f1e8faf283ab833b83d9d8
dredly <dredly@gmail.com>
2020-06-19 19:14:37 +0100

    exercise 3.5

--- part3/index.js
+++ part3/index.js
@@ -53,6 +53,30 @@ app.delete('/api/persons/:id', (request, response) => {
 	response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+	const body = request.body
+	
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * Math.floor(max))
+	}
+	
+	if (!body.name || !body.number) {
+		return response.status(400).json({
+			error: 'content missing'
+		})
+	}
+	
+	const person = {
+		name: body.name,
+		number: body.number,
+		id: getRandomInt(1000),
+	}
+	
+	persons = persons.concat(person)
+	
+	response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () =>  {
 	console.log(`Server running on ${PORT}`)
