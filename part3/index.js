497d99361b544f0a966e1fcda59575e3113175be
dredly <dredly@gmail.com>
2020-06-19 19:00:42 +0100

    exercise 3.4

--- part3/index.js
+++ part3/index.js
@@ -36,6 +36,23 @@ app.get('/info', (request, response) => {
 	response.send(numPeopleString + dateString)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+	response.json(person)
+  } else {
+	  response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	persons = persons.filter(person => person.id !== id)
+	response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () =>  {
 	console.log(`Server running on ${PORT}`)
