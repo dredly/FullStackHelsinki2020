5edbd54180d20be29d23b45d74eb596a541d1bf9
dredly <dredly@gmail.com>
2020-06-19 18:53:25 +0100

    Exercise 3.2

--- part3/index.js
+++ part3/index.js
@@ -30,6 +30,12 @@ app.get('/api/persons', (request, response) => {
 	response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+	const numPeopleString = `<p>Phonebook has info for ${persons.length} people</p>`
+	const dateString = `<p>${new Date()}</p>`
+	response.send(numPeopleString + dateString)
+})
+
 const PORT = 3001
 app.listen(PORT, () =>  {
 	console.log(`Server running on ${PORT}`)
