853ef0129813fb89e058e6802cd89d45415f02cc
dredly <dredly@gmail.com>
2020-07-13 17:27:52 +0100

    Exercise 3.9

--- part3b/index.js
+++ part3b/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 var morgan = require('morgan')
 const app = express()
 
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
   morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
